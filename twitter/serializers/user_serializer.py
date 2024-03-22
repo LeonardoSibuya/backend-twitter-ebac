a0baa97f84a2750eae37c0d988225c75e6ae4069
e_sibuya@outlook.com <e_sibuya@outlook.com>
2024-03-21 21:53:33 -0300

    FIX: Corrigido bug de multiplicar users

--- twitter/serializers/user_serializer.py
+++ twitter/serializers/user_serializer.py
@@ -13,14 +13,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'email', 'password', 'follows', 'followers', 'tweets']
         
     def get_follows(self, obj):
-        return obj.follows.values('id', 'name', 'email', 'follows', 'followers', 'tweet')
+        return obj.follows.values('id', 'name', 'email',)
     
     def get_followers(self, obj):
-        return obj.followers.values('id', 'name', 'email', 'follows', 'followers', 'tweet')
+        return obj.followers.values('id', 'name', 'email',)
+        
+    def get_tweets(self, obj):
+        tweets = obj.get_tweets()
+        tweet_data = []
+        for tweet in tweets:
+            tweet_serializer = TweetSerializer(tweet)
+            tweet_data.append(tweet_serializer.data)
+        return tweet_data
         
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        tweets = instance.get_tweets()  # Obtenha os tweets do usuário
-        tweet_serializer = TweetSerializer(tweets, many=True)  # Serialize os tweets
-        data['tweets'] = tweet_serializer.data  # Adicione os tweets serializados aos dados
+        data['tweets'] = self.get_tweets(instance)
         return data
