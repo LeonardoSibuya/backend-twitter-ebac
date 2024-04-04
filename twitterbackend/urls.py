55e6f3e753970fd3efd0bee74b553fb6e43a1891
e_sibuya@outlook.com <e_sibuya@outlook.com>
2024-04-04 18:11:33 -0300

    FEAT: Adicionando funcionalidade de chat

--- twitterbackend/urls.py
+++ twitterbackend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from twitter.viewsets import UserViewSet, TweetViewSet
 from rest_framework.routers import DefaultRouter
+from twitter.viewsets import ChatMessageViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -13,4 +14,6 @@ urlpatterns = [
     path('users/tweet/<int:tweet_id>/delete/', TweetViewSet.as_view({'delete': 'delete_tweet'}), name='tweet-delete'),
     path('users/<int:pk>/follow/<int:target_pk>', UserViewSet.as_view({'post': 'follow'}), name='user-follow'),
     path('users/<int:pk>/unfollow/<int:target_pk>', UserViewSet.as_view({'post': 'unfollow'}), name='user-unfollow'),
+    path('users/<int:user_id>/user_messages/', ChatMessageViewSet.as_view({'get': 'user_messages'}), name='user-messages'),
+    path('users/<int:user_id>/send_message/<int:receiver_id>/', ChatMessageViewSet.as_view({'post': 'send_message'}), name='send-message'),
 ]
